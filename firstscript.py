c4610b6046ded67a93136cc8924663f052d4a235
mantygi <manty291@gmail.com>
2026-02-22 11:08:37 +0530

    first v1 firstscript.py

---
+++ firstscript.py
@@ -0,0 +1,20 @@
+name="manty"
+n1=22
+n2=33
+lenth = 39
+width = 39
+print(f'area of rect {lenth * width}')
+print(f'my name is {name} , sum of two variables {n1+n2}')
+#this is comment
+'''this is multiline comment
+'''
+print(f'sum of two number are {10 + 23}')
+#list
+usr_ist=["manty","vishal","sahil","savan"]
+print(n1)
+print(type(usr_ist))
+print(n1*n2/n1+n2)
+msg=("this is the line of code")
+print(msg.split())
+usr_ist.reverse
+print(usr_ist)
